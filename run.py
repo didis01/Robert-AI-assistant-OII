845c415cbc4b799dea405dbffb7271f371a0063e
didis01 <xdidism@gmail.com>
2025-03-13 13:30:49 +0100

    update

--- run.py
+++ run.py
@@ -1,28 +1,6 @@
-import pyttsx3
-engine = pyttsx3.init() # object creation
+import os
+from playsound import playsound
 
-""" RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 200)     # setting up new voice rate
-
-
-"""VOLUME"""
-volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-print (volume)                          #printing current volume level
-engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-
-"""VOICE"""
-voices = engine.getProperty('voices')       #getting details of current voice
-#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
-
-engine.say("Hello World!")
-engine.say('My current speaking rate is ' + str(rate))
-engine.runAndWait()
-engine.stop()
-
-"""Saving Voice to a file"""
-# On linux make sure that 'espeak' and 'ffmpeg' are installed
-engine.save_to_file('Hello World', 'test.mp3')
-engine.runAndWait()
+os.system("sudo docker exec -ti piper-tts sh -c 'echo 'To address the elephant in the room: using text-to-speech technology isn’t just practical, it’s a lot of fun too!' | /opt/piper/build/piper --model /opt/piper/voices/aru/medium/en_GB-aru-medium.onnx --output_file /opt/speech.wav'")
+os.system("docker cp piper-tts:/opt/speech.wav ./")
+playsound("speech.wav")
