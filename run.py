4e65be4904ea9ef39fd7fa94ae8cd87e221f4ebf
didis01 <xdidism@gmail.com>
2025-03-14 11:26:22 +0100

    update

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from playsound import playsound
 
-os.system("sudo docker exec -ti piper-tts sh -c 'echo 'To address the elephant in the room: using text-to-speech technology isn’t just practical, it’s a lot of fun too!' | /opt/piper/build/piper --model /opt/piper/voices/aru/medium/en_GB-aru-medium.onnx --output_file /opt/speech.wav'")
-os.system("docker cp piper-tts:/opt/speech.wav ./")
+os.system("""sudo docker exec -ti piper-tts sh -c  + 'echo "To address the elephant in the room: using text-to-speech technology isn’t just practical, it’s a lot of fun too!" | /opt/piper/build/piper --model es_ES-davefx-medium.onnx --output_file /opt/speech.wav'""")
+os.system("sudo docker cp piper-tts:/opt/speech.wav ./")
 playsound("speech.wav")
