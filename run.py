82b9d77af5ef4b9700852ac2bee5cdc36dbf3765
didis01 <xdidism@gmail.com>
2025-03-14 13:13:16 +0100

    update

--- run.py
+++ run.py
@@ -3,11 +3,13 @@ from playsound import playsound
 import time
 
 
-message = "Buenos dias, mi noimbre es Robert, encantado de conocerte. Soy un robot de asistencia psicológica"
+message2 = "Hola, soy Robert, un robot del hogar. Y gracias a la inteligencia artificial, puedo ayudarte respecto a la soledad no deseada."
 
+message="Hola guapisima, ¿Cómo estás?"
 commands = """sudo docker exec -ti piper-tts sh -c  + 'echo "string&2" | /opt/piper/build/piper --model es_ES-davefx-medium.onnx --output_file /opt/speech.wav'"""
 
 os.system("sudo docker start piper-tts")
 os.system(commands.replace("string&2", message))
 os.system("sudo docker cp piper-tts:/opt/speech.wav ./")
+os.system("aplay notification.wav")
 os.system("aplay speech.wav")
