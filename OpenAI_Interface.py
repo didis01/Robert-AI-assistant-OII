322d05b1b494a89b8d6206afcc86a9001da46698
didis01 <xdidism@gmail.com>
2025-03-15 13:45:33 +0100

    update

--- OpenAI_Interface.py
+++ OpenAI_Interface.py
@@ -1,12 +1,23 @@
 from openai import OpenAI
-client = OpenAI()
+import os                                                                                                                                                                                                          
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+load_dotenv(Path(".env"))
+client = OpenAI(api_key=os.getenv("api_key"))
+
+system_prompt = "Eres un robot de asistencia para mayores, utilizas una raspberry pi 5, camaras y un lidar para funcionar. Tu idioma es el español. Tu objetivo es ayudar a las personas con la soledad no deseada, no estás hecho para reemplazar un humano, pero tienes que dar conversacion a tu usuario. Por favor, sé amable y educado. Si no sabes qué decir, puedes decir 'No sé qué decir', pero intenta dar siempre conversación."
+
 
 completion = client.chat.completions.create(
-  model="gpt-4o",
+  model="gpt-4o-mini-2024-07-18",
   messages=[
-    {"role": "developer", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
+    {"role": "system", 
+     "content": system_prompt
+    },
+
+    {"role": "user", 
+     "content": "Hola!"}
   ]
 )
-
-print(completion.choices[0].message)
+print(completion.choices[0].message.content)
