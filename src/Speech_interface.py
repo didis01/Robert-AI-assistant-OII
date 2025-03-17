cc0a747705441b2b3ad43fa587812fe7331b110c
didis01 <xdidism@gmail.com>
2025-03-17 15:27:06 +0100

    update

--- src/Speech_interface.py
+++ src/Speech_interface.py
@@ -5,6 +5,6 @@ def speak(text):
     commands = """sudo docker exec -ti piper-tts sh -c  + 'echo "string&2" | /opt/piper/build/piper --model es_ES-davefx-medium.onnx --output_file /opt/speech.wav'"""
     os.system("sudo docker start piper-tts")  # Inicia el contenedor Docker piper-tts
     os.system(commands.replace("string&2", text))  # Genera el audio con el texto proporcionado
-    os.system("sudo docker cp piper-tts:/opt/speech.wav ./")  # Copia el archivo de audio generado al sistema local
-    os.system("aplay /temp/notification.wav")  # Reproduce un sonido de notificación
-    os.system("aplay /temp/speech.wav")  # Reproduce el archivo de audio generado
+    os.system("sudo docker cp piper-tts:/opt/speech.wav ./temp/")  # Copia el archivo de audio generado al sistema local
+    os.system("aplay temp/notification.wav")  # Reproduce un sonido de notificación
+    os.system("aplay temp/speech.wav")  # Reproduce el archivo de audio generado
