0587bf41562d9ba99fad76de636301cb4c43b7c3
didis01 <xdidism@gmail.com>
2025-03-24 12:00:11 +0100

    update

--- src/Memory_interface.py
+++ src/Memory_interface.py
@@ -2,14 +2,14 @@ from OpenAI_Interface import getResponseFromOpenAi
 
 system_prompt = "Resume esta conversación. Ten en cuenta que tienes que preservar todos los detalles. Si no hay contenido para resumir, no digas nada, eres solo una maquina que resume."
 
-def Save_memory(user_input, robert_response, path="temp/LTM.txt"):
-    with open(path, 'a') as file:
+def Save_memory(user_input, robert_response, path="temp/LTM"):
+    with open(path + ".txt", 'a') as file:
         file.write("User: "+ user_input + "\nRobert: " + robert_response + "\n")
     
-    with open(path, 'r') as file:
+    with open(path + ".txt", 'r') as file:
         lmemory = file.read()
 
-    with open(path, 'w') as file:
+    with open(path + ".txt", 'w') as file:
         memory = getResponseFromOpenAi(lmemory, system_prompt)
         file.write(memory)
     
@@ -17,10 +17,10 @@ def Save_memory(user_input, robert_response, path="temp/LTM.txt"):
     return
 
 
-def Load_memory(path="temp/LTM.txt"):
-    with open(path, 'r') as file:
+def Load_memory(path="temp/LTM"):
+    with open(path + ".txt", 'r') as file:
         memory = file.read()
 
-    print("Memory loaded from ", path)
+    print("Memory loaded from ", path + ".txt")
     return memory
     
